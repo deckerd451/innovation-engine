e7adf06d5f6e7e179c3fd771a9503f7d0f328820
Doug Hamilton <Dmhamilton1@live.com>
2025-11-18 09:20:25 -0500

    Update cardRenderer.js

--- assets/js/cardRenderer.js
+++ assets/js/cardRenderer.js
@@ -4,6 +4,7 @@
 // =============================================================
 
 import { supabase } from "./supabaseClient.js";
+
 import { showNotification } from "./utils.js";
 
 // Creates user card HTML
