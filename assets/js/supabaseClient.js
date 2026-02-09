b8badde759b8ebe2dcfed704cf3caf9612171752
Doug Hamilton <Dmhamilton1@live.com>
2026-02-08 22:02:07 -0500

    Update Supabase client URL and key

--- assets/js/supabaseClient.js
+++ assets/js/supabaseClient.js
@@ -25,8 +25,8 @@ if (window.supabase) {
   // Create new client only if one doesn't exist
   try {
     supabaseInstance = createClient(
-      "https://hvmotpzhliufzomewzfl.supabase.co",
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2bW90cHpobGl1ZnpvbWV3emZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1NzY2NDUsImV4cCI6MjA1ODE1MjY0NX0.foHTGZVtRjFvxzDfMf1dpp0Zw4XFfD-FPZK-zRnjc6s",
+      "https://mqbsjlgnsirqsmfnreqd.supabase.co",
+      "sb_publishable_hKGoZiLtCe6BxgQjG23h2Q_hbGC-At3",
       {
         auth: {
           autoRefreshToken: true,
