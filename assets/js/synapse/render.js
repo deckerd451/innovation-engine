52024808b1dfed1d8ba07e8946333e21649f2588
Douglas Hamilton <YOUR_GITHUB_EMAIL@example.com>
2026-03-03 15:59:41 -0500

    fix: Correct comment block syntax in render.js
    Fixed malformed comment closing that caused SyntaxError at line 747.
    Changed 'END OF DISABLED THEME RENDERING CODE */' to proper comment syntax.

--- assets/js/synapse/render.js
+++ assets/js/synapse/render.js
@@ -824,7 +824,7 @@ export function renderThemeCircles(container, themeNodes, { onThemeHover, onThem
   }
 
   return d3.select(themesGroup.node());
-  END OF DISABLED THEME RENDERING CODE */
+  */ // END OF DISABLED THEME RENDERING CODE
 }
 
 // Function to highlight a selected theme
